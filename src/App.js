90d1b1cf04fcf0c71f4bf0ecdc30711bbab58188
amontalvof <amontalvof92@gmail.com>
2021-06-18 10:05:58 -0500

    feat: socket.io configuration in react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { AddBand } from './components/AddBand';
 import ListBands from './components/ListBands';
 
+const connectSocketServer = (params) => {
+    const socket = io.connect('http://localhost:8080', {
+        transport: ['websocket'],
+    });
+    return socket;
+};
+
 function App() {
+    const [socket] = useState(connectSocketServer());
+    const [online, setOnline] = useState(false);
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            setOnline(true);
+        });
+        socket.on('disconnect', () => {
+            setOnline(false);
+        });
+    }, [socket]);
+
     return (
         <div className="container">
             <div className="alert">
                 <p>
                     Service status:
-                    <span className="text-success"> Online</span>
-                    <span className="text-danger"> Offline</span>
+                    {online ? (
+                        <span className="text-success"> Online</span>
+                    ) : (
+                        <span className="text-danger"> Offline</span>
+                    )}
                 </p>
             </div>
             <h1>Bands</h1>
