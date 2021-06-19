9569334b043f2e21945126adff4137556503459a
amontalvof <amontalvof92@gmail.com>
2021-06-19 13:39:48 -0500

    feat: edit names in table

--- src/components/ListBands/index.js
+++ src/components/ListBands/index.js
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
-const CreateRows = ({ band }) => {
-    const { name } = band;
+const CreateRows = ({ band, onChange, onBlur }) => {
+    const { name, id, votes } = band;
     return (
         <tr>
             <td>
                 <button className="btn btn-primary">+1</button>
             </td>
             <td>
-                <input type="text" className="form-control" value={name} />
+                <input
+                    type="text"
+                    className="form-control"
+                    value={name}
+                    onChange={(event) => onChange(event, id)}
+                    onBlur={() => onBlur(id, name)}
+                />
             </td>
             <td>
-                <h3>15</h3>
+                <h3>{votes}</h3>
             </td>
             <td>
                 <button className="btn btn-outline-danger">Delete</button>
@@ -27,6 +33,23 @@ const ListBands = ({ data }) => {
         setBands(data);
     }, [data]);
 
+    const handleChange = (event, id) => {
+        const newName = event.target.value;
+        setBands(
+            bands.map((band) => {
+                if (band.id === id) {
+                    band.name = newName;
+                }
+                return band;
+            })
+        );
+    };
+
+    const handleBlur = (id, name) => {
+        console.log(id, name);
+        // TODO: disparar el evento de socket
+    };
+
     return (
         <>
             <table className="table table-stripped">
@@ -40,7 +63,14 @@ const ListBands = ({ data }) => {
                 </thead>
                 <tbody>
                     {bands.map((band) => {
-                        return <CreateRows key={band.id} band={band} />;
+                        return (
+                            <CreateRows
+                                key={band.id}
+                                band={band}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        );
                     })}
                 </tbody>
             </table>
